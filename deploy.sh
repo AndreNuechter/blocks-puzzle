9573dbdc07a4c232aa75e22aef7b1a97525fd404
AN <andre_nuechter@hotmail.de>
2021-06-06 21:41:26 +0200

    Tinker with deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-deno run --unstable --allow-read --allow-write --allow-run build.js && git add docs/. && git commit --amend --no-edit && git push --all
+git stash --all && deno run --unstable --allow-read --allow-write --allow-run build.js && git add docs/. && git commit --amend --no-edit && git push --all && git stash pop
