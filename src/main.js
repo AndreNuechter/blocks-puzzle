814ac0224cd85c1a87bdfd1a55476aea19858aa0
AN <andre_nuechter@hotmail.de>
2021-07-05 00:20:06 +0200

    Add pixel font

--- src/main.js
+++ src/main.js
@@ -3,10 +3,8 @@ import './js/wakelock.js';
 import './js/input-handling.js';
 
 // TODO show input options on splashscreen
-// TODO reset
-// TODO high scores
-// TODO give canvases label
-// TODO change cache-id on build
-// TODO delete outdated caches
+// TODO reset btn
+// TODO high scores on game over screen
+// TODO add labels to canvases
+// TODO automatically change cache-id on build
 // TODO border on pieces may be cut off on small displays
-// TODO pixelfont https://fonts.google.com/specimen/VT323
