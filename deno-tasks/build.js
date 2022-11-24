e7db99b3e324811b76c7aa37abae784dc251f2de
Andre Nuechter <andre_nuechter@hotmail.de>
2022-11-24 13:10:41 +0100

    Prevent minify neutering the js-bundle

--- deno-tasks/build.js
+++ deno-tasks/build.js
@@ -1,10 +1,8 @@
 import { emptyDir, copy } from "https://deno.land/std@0.95.0/fs/mod.ts";
-import { Language, minify } from "https://deno.land/x/minifier/mod.ts";
+import { Language, minify } from "https://deno.land/x/minifier@v1.1.1/mod.ts";
 
 // clear docs folder or create it if it doesnt exist
 await emptyDir("./docs");
-
-// TODO it may be possible to directly minify uint8array (which is returned by the run)... minify also has a cli so we could propably do all this in .sh
 // minify html and css into docs
 const html = await Deno.readTextFile("./src/index.html");
 await minifyAndMoveFile("./docs/index.html", "HTML", html);
@@ -15,7 +13,11 @@ const js = new TextDecoder().decode(await Deno.run({
     cmd: ["deno", "bundle", "./src/main.js"],
     stdout: "piped"
 }).output());
-await minifyAndMoveFile("./docs/main.js", "JS", js);
+// NOTE: we manually replace inline-comments,
+// as deno.bundle adds inline-comments to the start of its output, 
+// which minify doesnt remove, 
+// making the bundle a long comment
+await minifyAndMoveFile("./docs/main.js", "JS", js.replace(/^\/\/[^\n\r]+[\n\r]*$/gm, ''));
 // minify service-worker
 const sw = await Deno.readTextFile("./src/service-worker.js");
 await minifyAndMoveFile("./docs/service-worker.js", "JS", sw.replace('#!#', Date.now()));
